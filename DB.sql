bfc3d72dfd0f51ce2427e075ebb5da0d0d5d78aa
znix <64723880+znixhook@users.noreply.github.com>
2020-10-24 10:28:22 +0530

    Added SQL file

---
+++ DB.sql
@@ -0,0 +1,49 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+DROP TABLE IF EXISTS `cheat`;
+CREATE TABLE IF NOT EXISTS `cheat` (
+  `status` int(1) NOT NULL DEFAULT 0,
+  `version` int(1) NOT NULL DEFAULT 0,
+  `maintenance` int(1) NOT NULL DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `invites`;
+CREATE TABLE IF NOT EXISTS `invites` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) NOT NULL,
+  `used` int(1) NOT NULL DEFAULT 0,
+  `createdBy` varchar(255) NOT NULL,
+  `createdAt` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `code` (`code`),
+  UNIQUE KEY `uid` (`uid`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `hwid` varchar(255) DEFAULT NULL,
+  `admin` int(1) NOT NULL DEFAULT 0,
+  `active` int(1) NOT NULL DEFAULT 0,
+  `banned` int(1) NOT NULL DEFAULT 0,
+  `invitedBy` varchar(255) NOT NULL,
+  `createdAt` timestamp NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `uid` (`uid`),
+  UNIQUE KEY `hwid` (`hwid`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+COMMIT;
+
+INSERT INTO `users` (`uid`, `username`, `password`, `hwid`, `admin`, `active`, `banned`, `invitedBy`, `createdAt`) VALUES
+(1, 'admin', '$2y$10$X3K9Vm/RBWfTl.drTi4//OtiyB.s9uaRmia0q2sLzjaET.im3sfvu', NULL, 1, 1, 0, '', '2020-10-20 08:29:37');
+COMMIT;
+
+INSERT INTO `cheat` (`status`, `version`, `maintenance`) VALUES
+(0, 1, 0);
+COMMIT;
